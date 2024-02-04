f39fa3aba2cb8852fde4d6687ea44e338602c05a
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-02-04 08:54:47 -0300

    create: create new route /

--- routes.js
+++ routes.js
@@ -1,12 +1,14 @@
 import app from "./config/config.js"
 import MySQlConnect from "./db/database.js"
 import express from "express"
+import queryIpFunction from "./models/ip.js"
 
 
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.send('Hello World')
+app.get('/', async(req, res)=>{
+    const ip = await queryIpFunction()
+    res.json(ip.ip)
 })
